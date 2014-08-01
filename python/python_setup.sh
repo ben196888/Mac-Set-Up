e91a84406a5981990c507b9c0bedeaee48e62a4c
GPS <gps@GPS-MacBookPro.local>
2014-08-01 11:19:49 +0800

    Add python setup

---
+++ python/python_setup.sh
@@ -0,0 +1,22 @@
+#Python
+#Setup shell
+
+#In virtualenv ENV
+echo "You should be in virtual env!"
+#ipython
+pip install ipython
+
+#bpython
+pip install bpython
+
+#splinter
+pip install splinter
+
+#robot framework
+pip install robotframework
+
+#beautifulsoup
+pip install beautifulsoup
+
+#webtest
+pip install webtest
