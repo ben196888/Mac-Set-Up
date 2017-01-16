88514ec861240786d00fd137469fb59836f3f5cb
ben196888 <ben196888@gmail.com>
2017-01-16 16:20:41 +1100

    Update yarn global add

--- node/installation.sh
+++ node/installation.sh
@@ -3,6 +3,14 @@
 # Upgrade npm
 npm install -g npm
 
+# Use yarn instead of npm
 # Install react developing environment
-npm install -g create-react-app
-npm install -g webpack
+#npm install -g create-react-app
+yarn global create-react-app
+#npm install -g webpack
+yarn global add webpack
+yarn global add cross-env
+yarn global add webpack-dev-server
+yarn global add eslint
+yarn global add mocha
+yarn global add gh-page
