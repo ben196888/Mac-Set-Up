d4378559aef3f1ed9ef739a18d948c96e4d37b4e
Ben Liu <ben196888@gmail.com>
2017-10-26 15:12:54 +1100

    Scripted all installation.

---
+++ installation.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Install Xcode Command Line Tools
+xcode-select --install
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install gui apps through brew cask
+cd brew-cask
+./installation.sh
+cd ..
+
+# Install cmd line tools through brew
+cd brew
+./installation.sh
+cd ..
+
+# Install node global modules
+cd node
+./installation.sh
+cd ..
+
+# Install atom packages and themes
+cd atom
+./installation.sh
+cd ..
+
+# Setup fish
+echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
+mkdir -p ~/.config/fish
+touch ~/.config/fish/config.fish
