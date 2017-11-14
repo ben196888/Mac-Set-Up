fb38f1e5a8b1d3dae616c5c2f37b846b6ca07c0d
Ben Liu <ben.liu@tabcorp.com.au>
2017-11-15 10:27:23 +1100

    Add work related installation pkg.

---
+++ workInstallation.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Install Tabcorp related
+brew install maven
+brew install typesafe-activator
+brew install leiningen
+brew install scala
+brew install sbt
+
+brew cask install silverlight
+brew cask install vip-access
+brew cask install mysqlworkbench
+
