9d9158ac1c897cf3b4a3fb6348059c14d733a0f6
Ben Liu <ben.liu@tabcorp.com.au>
2017-12-01 12:11:25 +1100

    Install docker-machine-driver-xhyve

--- brew/installation.sh
+++ brew/installation.sh
@@ -21,6 +21,12 @@ mkdir $HOME/Go
 mkdir -p $HOME/Go/src/github.com/ben196888
 brew install go
 
+# Install docker-machine-driver-xhyve
+brew install docker-machine-driver-xhyve
+# docker-machine-driver-xhyve need root owner and uid
+sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+
 # Install optimized tools
 brew install ripgrep
 
