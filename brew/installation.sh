0d5423f100efe080da703059c8c92d8a8baca150
Ben Liu <ben.liu@tabcorp.com.au>
2018-01-11 11:03:20 +1100

    Add gettext for envsubt

--- brew/installation.sh
+++ brew/installation.sh
@@ -28,6 +28,9 @@ brew install docker-machine-driver-xhyve
 sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
 sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
 
+# Install envsubt tool
+brew install gettext
+
 # Install optimized tools
 brew install ripgrep
 
