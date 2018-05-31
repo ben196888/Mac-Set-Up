75880da4d1d4bb5dc038c32b0e8fda8c9c8b7305
Ben Liu <ben196888@gmail.com>
2018-05-31 11:23:49 +1000

    Add gitmoji-cli and update installation file.

--- node/installation.sh
+++ node/installation.sh
@@ -7,16 +7,31 @@ npm install -g npm
 # Install react developing environment
 yarn global add create-react-app
 yarn global add create-react-native-app
-yarn global add webpack
+
+# Environment
 yarn global add cross-env
+
+# Packaging/Bundle
+yarn global add bower
+yarn global add webpack
 yarn global add webpack-dev-server
+
+# Coding style
 yarn global add eslint
 yarn global add editorconfig-tools
 yarn global add eslint-to-editorconfig
+
+# Test
 yarn global add mocha
 yarn global add jest
+
+# Deploy
 yarn global add gh-pages
-yarn global add bower
+
+# Typescript
 yarn global add typescript
 yarn global add typings
+
+# Other
+yarn global add gitmoji-cli
 
