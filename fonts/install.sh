54d100cb2881b6adb3c5818bb4b1e94aea653ad3
Ben Liu <ben196888@gmail.com>
2019-11-07 21:17:04 +0700

    Install powerline fonts

---
+++ fonts/install.sh
@@ -0,0 +1,11 @@
+
+# Install powerline fonts
+
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1 powerlinefonts
+# install
+cd powerlinefonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf powerlinefonts
