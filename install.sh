1ae31015d2a11e664451b92741c07cf673f0b8f8
Ben Liu <ben196888@gmail.com>
2020-09-11 17:58:57 +0700

    fix: xcode and brew

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-xcode-select --install && # Install Xcode Command Line Tools
+xcode-select --install || true && # Install Xcode Command Line Tools
 sudo xcode-select --switch /Library/Developer/CommandLineTools && # node-gyp rebuild issue. Enable command lint tools and ignore the warnings.
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && # Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" && # Install Homebrew
 ./brew/install.sh && # Install cmd line tools through brew
 ./brew-cask/install.sh && # Install gui apps through brew cask
 ./zsh/install.sh && # Install zsh, oh my zsh
