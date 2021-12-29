96aaf7c31e4fbfe3f7adeba28b578e9d31104147
Ben Liu <ben196888@gmail.com>
2021-12-29 12:13:58 +0700

    feat: add rosetta to use apps built for intel processor

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ xcode-select --install || true
 
 # node-gyp rebuild issue. Enable command lint tools and ignore the warnings.
 sudo xcode-select --switch /Library/Developer/CommandLineTools
+
+# Rosetta 2 enables a Mac with Apple silicon to use apps built for a Mac with an Intel processor.
+softwareupdate --install-rosetta
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 # Install cmd line tools through brew
