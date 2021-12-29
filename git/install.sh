a6efd5b22d94abc58be552c6bd481ccd8986f536
Ben Liu <ben196888@gmail.com>
2021-12-29 09:51:57 +0700

    feat: add git ssh key setup

--- git/install.sh
+++ git/install.sh
@@ -7,3 +7,17 @@ BASEDIR=$(dirname "$0")
 
 cp "$BASEDIR/.gitconfig" "$HOME/.gitconfig"
 cp "$BASEDIR/.gitignore_global" "$HOME/.gitignore_global"
+
+ssh-keygen -t ed25519 -C "ben196888@gmail.com" -f "$HOME/.ssh/id_github"
+touch ~/.ssh/config
+cat <<EOF >> ~/.ssh/config
+Host *
+  AddKeysToAgent yes
+  UseKeychain yes
+  IdentityFile ~/.ssh/id_github
+EOF
+ssh-add --apple-use-keychain ~/.ssh/id_github
+
+echo "==== Add new SSH key in github ===="
+cat ~/.ssh/id_github.pub
+echo "==================================="
