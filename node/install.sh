8596b5107f1c7952bfb121ea215de223d787370c
Ben Liu <ben196888@gmail.com>
2021-12-30 01:49:59 +0700

    fix: n requires /usr/local/ permission

--- node/install.sh
+++ node/install.sh
@@ -2,7 +2,17 @@
 
 # Install Nodejs
 brew install node
+
+# install n for node version management
 brew install n
+# make cache folder (if missing) and take ownership
+sudo mkdir -p /usr/local/n
+sudo chown -R $(whoami) /usr/local/n
+# make sure the required folders exist (safe to execute even if they already exist)
+sudo mkdir -p /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+# take ownership of Node.js install destination folders
+sudo chown -R $(whoami) /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+
 brew install yarn
 brew install watchman
 brew install deno
