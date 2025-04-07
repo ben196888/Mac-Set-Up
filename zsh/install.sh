fe8e9dcd5e783bc21f329f22a3f3979c4a7393ee
Ben Liu <ben196888@gmail.com>
2025-04-07 21:18:58 +0700

    fix(zsh): install oh my zsh conditionally

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,9 +4,14 @@ set -e
 echo "Installing essential tools..."
 brew install zsh starship font-hack-nerd-font
 
-echo "Installing Oh My Zsh..."
-export RUNZSH=no
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install Oh My Zsh only if not already installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Installing Oh My Zsh..."
+  export RUNZSH=no
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echo "Oh My Zsh is already installed. Skipping..."
+fi
 
 echo "Copying .zshrc to home directory..."
 cp "$(dirname "$0")/.zshrc" "$HOME/.zshrc"
